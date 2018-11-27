4c2c33a6b05cae873f9663751b042c07913a351e
dmahfouda <david.mahfouda@gmail.com>
2018-11-27 12:57:16 -0500

    sending messages and saving them with update to can pair

--- index.js
+++ index.js
@@ -1,24 +1,120 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express = require('express')
+var app = express()
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.use(express.static('build'))
+
+const mongoose = require('mongoose')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/cans')
+
+let db = mongoose.connection
+db.on('error', console.error.bind(console, 'connection error:'))
+db.once('open', function(callback) {
+  console.log('database connected')
+  mongoose.connection.db.listCollections().toArray((err, names) => {
+    console.log(`collection names: ${names}`); // [{ name: 'dbname.myCollection' }]
+  })
 })
+
+let canSchema = new mongoose.Schema({
+  canName: String,
+  messages: Array
+})
+
+let pairs = {
+  'bloopy': 'blimpy',
+  'blimpy': 'bloopy'
+}
+
+// let canPairSchema = new mongoose.Schema({
+//   can1: String,
+//   can2: String
+// }, {collection :'canPairs'})
+
+let Can = mongoose.model('Can', canSchema)
+// let CanPairs = mongoose.model('canPair', canPairSchema, 'canPairs')
+
+// CanPairs.find({}, (err, data) => {
+//   if (!err) {
+//     console.log(`data: ${data.json}`)
+//   }
+// })
+
+var client
 
 io.on('connection', function(socket){
   console.log('a user connected');
-  	// socket.use((packet, next) => {
-  	// 	console.log(packet);
-  	// 	next();
-  	// })
-  socket.on('messageType', () => { 
-  	console.log('received message')
+  
+  socket.on('messageType', (msg) => { 
+    var data = new Uint8Array(msg.sample);
+    if (client) {
+      client.emit('audio', data)
+    }
+    // player.feed(data);
   })
-  socket.send('hello')
+
+  socket.on('initialConnection', (msg) => {
+    console.log('initialConnection')
+    Can.find( msg, (err, cans) => {
+      if (err) { 
+        return console.error(err)
+      } else {
+          if (cans.length == 0) {
+            console.log('no can in database')
+            console.log(cans)
+            let can = new Can(msg)
+            can.save( err => {
+                if (err) {
+                  console.log(err)
+                } else {
+                  console.log('we think we saved it')
+                }
+              })
+          } else {
+            console.log('this is where we will send back can state from server')
+            console.log(`cans: ${cans}`)
+          }
+      }
+    })
+  })
+
+  socket.on('incomingMessage', (msg) => {
+    console.log(`msg.name: ${msg.name}`)
+    console.log(`pairs[msg.name]: ${pairs[msg.name]}`)
+    Can.update({canName:pairs[msg.name]}, {messages:[msg.message]}, (err, raw) => {
+      // console.log(`cans2: ${cans}`)
+      if (!err) {
+        // cans.messages.push(msg.message)
+        // cans.save(err => {
+        //   if (!err) {
+        //     console.log('we think we saved a message')
+        //   }
+        // })
+        console.log(raw)
+      } 
+      // else {
+        // console.log('no find error') 
+      // } 
+    }) 
+  })
+  
+  // socket.send('hello')
 	socket.on('disconnect', function(){
 		console.log('disconnected: ' + socket.id);
 	})
+
+  socket.on('chat message', function(msg){
+    // console.log('message: ' + msg);
+    io.send(msg)
+  })
+
+  socket.on('register', () => {
+    console.log('registered');
+    client = socket.conn
+    // socket.emit('ack')
+  })
+  
   //ping();
 })
 
--- socketIOClient/socketIOClient.ino
+++ socketIOClient/socketIOClient.ino
@@ -27,6 +27,11 @@ uint64_t messageTimestamp = 0;
 uint64_t heartbeatTimestamp = 0;
 bool isConnected = false;
 
+//chip states
+String on = "on";
+String off = "off";
+int messageState = 0;
+
 void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
 
 
@@ -46,14 +51,26 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
             }
             break;
         case WStype_TEXT:
-            USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+            // USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+
+            // USE_SERIAL.printf((char*)payload);
+            // USE_SERIAL.printf("%s\n",payload[1]);
+            
+            if (on.equals((char*)payload)) {
+                messageState = 1;
+                // USE_SERIAL.printf("turned on" );
+            }
+            if (off.equals((char*)payload)) {
+                messageState = 0;
+                // USE_SERIAL.printf("turned off" );
+            }
 
 			// send message to server
 			// webSocket.sendTXT("message here");
             break;
         case WStype_BIN:
-            USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
-            hexdump(payload, length);
+            // USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+            // hexdump(payload, length);
 
             // send data to server
             // webSocket.sendBIN(payload, length);
@@ -61,6 +78,14 @@ void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
     }
 
 }
+
+const int mvMax = 100;
+
+// if sampleWindow == 50, barGraph printing doesn't work correctly
+// Sample window width in mS (50 mS = 20 Hz)
+const int sampleWindow = 10;
+
+unsigned int sample;
 
 void setup() {
     // USE_SERIAL.begin(921600);
@@ -91,29 +116,99 @@ void setup() {
     webSocket.beginSocketIO("10.0.20.109", 3000);
     //webSocket.setAuthorization("user", "Password"); // HTTP Basic Authorization
     webSocket.onEvent(webSocketEvent);
-
+    pinMode(LED_BUILTIN, OUTPUT);
 }
 
 bool sent = false;
+bool sentInitialConnection = false;
+
+const String CAN_NAME = "bloopy";
+
+int counter = 0;
 
 void loop() {
     webSocket.loop();
 
+    counter++;
+
     if(isConnected) {
+        if(!sentInitialConnection) {
+            String initialConnectionText = "42[\"initialConnection\", { \"canName\" : \"" + CAN_NAME + "\" }]";
+            if(webSocket.sendTXT(initialConnectionText)) {
+                USE_SERIAL.println("sent initial connection request successfully");
+                sentInitialConnection = true;
+            } else {
+                USE_SERIAL.println("failed to send initial connection request");
+            }
+        }
 
         uint64_t now = millis();
-
         if(now - messageTimestamp > MESSAGE_INTERVAL) {
             messageTimestamp = now;
-            USE_SERIAL.printf("sending socket message\n");
+            // USE_SERIAL.printf("sending socket message\n");
             // example socket.io message with type "messageType" and JSON payload
-            webSocket.sendTXT("42[\"messageType\",{\"greeting\":\"hello\"}]");
+            // webSocket.sendTXT("42[\"messageType\",{\"greeting\":\"hello\"}]");
         }
         if((now - heartbeatTimestamp) > HEARTBEAT_INTERVAL) {
             heartbeatTimestamp = now;
             // socket.io heartbeat message
-            USE_SERIAL.printf("heartbeat\n");
+            // USE_SERIAL.printf("heartbeat\n");
             webSocket.sendTXT("2");
         }
+    
+        //Audio stuff
+        unsigned long startMillis= millis();  // Start of sample window
+        unsigned int peakToPeak = 0;   // peak-to-peak level
+
+        unsigned int signalMax = 0;
+        unsigned int signalMin = 1024;    
+
+        // collect data for sampleWindow mS
+        while (millis() - startMillis < sampleWindow)
+        {
+            sample = analogRead(0);
+            if (sample < 1024)  // toss out spurious readings
+            {
+                if (sample > signalMax)
+                {
+                    signalMax = sample;  // save just the max levels
+                }
+                else if (sample < signalMin)
+                {
+                    signalMin = sample;  // save just the min levels
+                }
+            }
+        }
+
+        peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+        // convert to volts
+        //double volts = (3.3 * peakToPeak) / 1024;
+        // convert to millivolts
+        // int millivolts = int(1000 * (3.3 * peakToPeak) / 1024);
+
+        String c = String(peakToPeak, DEC);
+        String d = "42[\"fromArduino\",{\"sample\":\""+c+"\"}]";
+        webSocket.sendTXT(d);
+        // webSocket.sendBIN(&peakToPeak, sizeof(unsigned int));
+        Serial.println(peakToPeak);
+
+        if (counter%1000 == 0) {
+            String messageText = "42[\"incomingMessage\", {\"name\": \"bloopy\", \"message\": \"whats up?????\"}]";
+            webSocket.sendTXT(messageText);
+            USE_SERIAL.printf("sent whats up message\n");
+        }
+
     }
+
+    switch(messageState) {
+        case 0:
+            digitalWrite(LED_BUILTIN, LOW);
+            break;
+        case 1:
+            digitalWrite(LED_BUILTIN, HIGH);
+            break;
+    }
+
+
+
 }
