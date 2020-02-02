1435ee6b2b595b767f5114424bd86fce0041559f
Michael Goldenberg <m@mgoldenberg.net>
2020-02-02 15:57:03 -0500

    move some things around

---
+++ .archive/audioInput/audioInput.ino
@@ -0,0 +1,80 @@
+/****************************************
+  Example Sound Level Sketch for the 
+  Adafruit Microphone Amplifier
+http://learn.adafruit.com/adafruit-microphone-amplifier-breakout/measuring-sound-levels
+ ****************************************/
+
+// Pin 13 has an LED connected
+const int led = LED_BUILTIN;
+
+const int mvMax = 100;
+
+// if sampleWindow == 50, barGraph printing doesn't work correctly
+// Sample window width in mS (50 mS = 20 Hz)
+const int sampleWindow = 10;
+
+unsigned int sample;
+
+int barLength = 0;
+const int barLengthTotal = 120;
+char barGraph[barLengthTotal] = "";
+
+void setup() 
+{
+    Serial.begin(9600);
+    // initialize the digital pin as an output.
+    pinMode(led, OUTPUT);
+}
+
+
+void loop() 
+{
+    unsigned long startMillis= millis();  // Start of sample window
+    unsigned int peakToPeak = 0;   // peak-to-peak level
+
+    unsigned int signalMax = 0;
+    unsigned int signalMin = 1024;
+
+    // collect data for sampleWindow mS
+    while (millis() - startMillis < sampleWindow)
+    {
+        sample = analogRead(0);
+        if (sample < 1024)  // toss out spurious readings
+        {
+            if (sample > signalMax)
+            {
+                signalMax = sample;  // save just the max levels
+            }
+            else if (sample < signalMin)
+            {
+                signalMin = sample;  // save just the min levels
+            }
+        }
+    }
+
+    peakToPeak = signalMax - signalMin;  // max - min = peak-peak amplitude
+    // convert to volts
+    //double volts = (3.3 * peakToPeak) / 1024;
+    // convert to millivolts
+    int millivolts = int(1000 * (3.3 * peakToPeak) / 1024);
+
+    Serial.println(millivolts);
+    
+    if (millivolts > mvMax) {
+        // turn the LED on (HIGH is the voltage level)
+        digitalWrite(led, HIGH);
+    } else {
+        // turn the LED off by making the voltage LOW
+        digitalWrite(led, LOW);
+    }
+
+    barLength = int(millivolts/10);
+    for (int i = 0; i < barLengthTotal; i++) {
+        if (i <= barLength) {
+            barGraph[i] = '*';
+        } else {
+            barGraph[i] = ' ';
+        }
+    }
+    Serial.println(barGraph);
+}
---
+++ .archive/mp3Test/mp3Test.ino
@@ -0,0 +1,30 @@
+#include <Arduino.h>
+#include "AudioFileSourceSPIFFS.h"
+#include "AudioGeneratorMP3.h"
+#include "AudioOutputI2SNoDAC.h"
+
+AudioGeneratorMP3 *mp3;
+AudioFileSourceSPIFFS *file;
+AudioOutputI2SNoDAC *out;
+void setup()
+{
+  Serial.begin(115200);
+  Serial.printf("setup");
+  delay(1000);
+  SPIFFS.begin();
+  file = new AudioFileSourceSPIFFS("/jamonit.mp3");
+  out = new AudioOutputI2SNoDAC();
+  mp3 = new AudioGeneratorMP3();
+  mp3->begin(file, out);
+}
+
+void loop()
+{
+  if (mp3->isRunning()) {
+    if (!mp3->loop()) mp3->stop(); 
+      Serial.printf("running");
+  } else {
+    Serial.printf("MP3 done\n");
+    delay(1000);
+  }
+}
