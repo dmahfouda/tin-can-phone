e39f4696c827340adc3a00b129bb655618833de7
dmahfouda <david.mahfouda@gmail.com>
2020-03-23 20:46:25 -0400

    specifying int in config sample

--- Config.sample.h
+++ Config.sample.h
@@ -2,4 +2,4 @@
 #define TIN_CAN_PASSWORD "<wifi-password>"
 #define TIN_CAN_ID "<can-name>"
 #define TIN_CAN_SWITCH_HOST "<local ip address>"
-#define TIN_CAN_SWITCH_PORT "<local port>"
+#define TIN_CAN_SWITCH_PORT <INT local port>
